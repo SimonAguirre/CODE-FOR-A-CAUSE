e7a8b6acd61426a07b080467f2ea9142ab2ce581
Simon Ken <asimonken@gmail.com>
2024-02-18 19:10:30 +0800

    added backend sourcecode

---
+++ Project1/animelist/anime/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Anime(models.Model):
+    name = models.CharField(max_length=150)
+    genre = models.CharField(max_length=200)
+    rating = models.IntegerField()
+    favorite = models.BooleanField()
+    comment = models.CharField(max_length=1000)
+    current_episode = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Animes(models.Model):
+    name = models.CharField(max_length=200)
+    genres = models.CharField(max_length=200)
+    num_of_eps = models.IntegerField()
+    img = models.CharField(max_length=200)
+
+admin.site.register(Anime)
+admin.site.register(Animes)
